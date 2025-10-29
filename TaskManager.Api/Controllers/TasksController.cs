35d3272c714b728015bcb56fe6f30a54a7d0987f
Vladyslav_R <98883690+vylradas@users.noreply.github.com>
2025-10-29 15:16:43 +0200

    Add TasksController with basic CRUD endpoints

---
+++ TaskManager.Api/Controllers/TasksController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManager.Api.Models;
+using TaskManager.Api.Repositories;
+
+namespace TaskManager.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController : ControllerBase
+    {
+        private readonly TaskRepository _repository;
+
+        public TasksController(TaskRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<TaskItem>> GetAll() => Ok(_repository.GetAll());
+
+        [HttpGet("{id}")]
+        public ActionResult<TaskItem> Get(Guid id)
+        {
+            var task = _repository.GetById(id);
+            if (task == null) return NotFound();
+            return Ok(task);
+        }
+
+        [HttpPost]
+        public ActionResult<TaskItem> Create(TaskItem task)
+        {
+            _repository.Add(task);
+            return CreatedAtAction(nameof(Get), new { id = task.Id }, task);
+        }
+    }
+}
