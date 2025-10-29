8254bcf46d7f2ad5c3471afe26dd648eca9ede73
Vladyslav_R <98883690+vylradas@users.noreply.github.com>
2025-10-29 15:08:10 +0200

    Add in-memory TaskRepository

---
+++ TaskManager.Api/Repositories/TaskRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using TaskManager.Api.Models;
+
+namespace TaskManager.Api.Repositories
+{
+    public class TaskRepository
+    {
+        private readonly List<TaskItem> _tasks = new();
+
+        public IEnumerable<TaskItem> GetAll() => _tasks;
+
+        public TaskItem? GetById(Guid id) => _tasks.FirstOrDefault(t => t.Id == id);
+
+        public void Add(TaskItem task) => _tasks.Add(task);
+
+        public void Update(TaskItem task)
+        {
+            var index = _tasks.FindIndex(t => t.Id == task.Id);
+            if (index >= 0)
+            {
+                _tasks[index] = task;
+            }
+        }
+    }
+}
