e9bfa642fe890e562de7273de0a9da3714ddd19f
Vladyslav_R <98883690+vylradas@users.noreply.github.com>
2025-10-29 14:55:53 +0200

    Add TaskItem model with default status

--- TaskManager.Api/Models/TaskItem.cs
+++ TaskManager.Api/Models/TaskItem.cs
@@ -1,6 +1,18 @@
 ﻿namespace TaskManager.Api.Models
 {
+    public enum TaskStatus
+    {
+        Backlog,
+        InWork,
+        Testing,
+        Done
+    }
+
     public class TaskItem
     {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public TaskStatus Status { get; set; } = TaskStatus.Backlog;
     }
 }
